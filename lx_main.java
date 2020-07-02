fcffdfacd4e06cb32b2f9cd24d6f8c1237fa7bd4
luolei <1726420180@qq.com>
2020-07-02 20:45:16 +0800

    stash冲突解决,删除修改，恢复现场

--- lx_main.java
+++ lx_main.java
@@ -2,6 +2,6 @@ public class lxapp(){
   public void Register(User user){}
   public void Login(String username, String passwd){}
   public void ModifyPassword(String uname, String passwd1, String passwd2){
-    //开发中。
+      //开发中。
   }
 }
