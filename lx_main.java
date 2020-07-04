f9639b85946a49d2dd24ed2a20323e0843642346
lei <44152650+githubluolei@users.noreply.github.com>
2020-07-04 10:16:40 +0800

    Update lx_main.java

--- lx_main.java
+++ lx_main.java
@@ -1,5 +1,7 @@
 public class lxapp(){
   public void Register(User user){}
   public void Login(String username, String passwd){}
-  public void ModifyPassword(String uname, String passwd1, String passwd2);
+  public void ModifyPassword(String uname, String passwd1, String passwd2){
+     //Development completed
+  }
 }
